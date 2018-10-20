3738e931b547e2ac5261fa24f300ec5d9e216767
lchoo <lchoo1996@gmail.com>
2018-10-20 13:02:39 -0400

    feat: added seeker and professional in route for sign in/up profile

--- web/routes/signUp.js
+++ web/routes/signUp.js
@@ -1,38 +1,64 @@
-// const express = require("express");
-// const path = require("path");
-// const bodyParser = require("body-parser");
-// const request = require("request");
-// // const crypto = require("crypto-js/sha3");
-// const router = express.Router();
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const request = require("request");
+const crypto = require("crypto-js/sha3");
+const router = express.Router();
 
-// router.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/html/signIn.html"));
-// });
+router.get("/seeker", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/signInSeeker.html"));
+});
 
-// router.post("/", function(req, res) {
-//     var data = {
-//         email: req.body.email,
-//         password: crypto(req.body.email + req.body.password).toString(),
-//     };
+router.get("/professional", function(req, res) {
+    res.sendFile(
+        path.join(__dirname, "../public/html/signInProfessional.html"),
+    );
+});
 
-//     console.log(data);
+router.post("/seeker", function(req, res) {
+    //     var data = {
+    //         email: req.body.email,
+    //         password: crypto(req.body.email + req.body.password).toString(),
+    //     };
+    //     console.log(data);
+    //     var options = {
+    //         uri: "http://localhost:8080/user/login",
+    //         json: data,
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //     };
+    //     request(options, function(error, response) {
+    //         // global.cookie = response.headers["set-cookie"];
+    //         if (response) {
+    //             res.send(response.body);
+    //         }
+    //         return;
+    //     });
+});
 
-//     var options = {
-//         uri: "http://localhost:8080/user/login",
-//         json: data,
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//     };
+router.post("/professional", function(req, res) {
+    //     var data = {
+    //         email: req.body.email,
+    //         password: crypto(req.body.email + req.body.password).toString(),
+    //     };
+    //     console.log(data);
+    //     var options = {
+    //         uri: "http://localhost:8080/user/login",
+    //         json: data,
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //     };
+    //     request(options, function(error, response) {
+    //         // global.cookie = response.headers["set-cookie"];
+    //         if (response) {
+    //             res.send(response.body);
+    //         }
+    //         return;
+    //     });
+});
 
-//     request(options, function(error, response) {
-//         // global.cookie = response.headers["set-cookie"];
-//         if (response) {
-//             res.send(response.body);
-//         }
-//         return;
-//     });
-// });
-
-// module.exports = router;
+module.exports = router;
