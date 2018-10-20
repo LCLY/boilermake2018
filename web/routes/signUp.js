4bd01cf646cd7bf944b2ece16599f1324b75ef70
lchoo <lchoo1996@gmail.com>
2018-10-20 10:57:15 -0400

    chore: router files index, signIn, signUp

---
+++ web/routes/signUp.js
@@ -0,0 +1,38 @@
+// const express = require("express");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+// const request = require("request");
+// // const crypto = require("crypto-js/sha3");
+// const router = express.Router();
+
+// router.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/html/signIn.html"));
+// });
+
+// router.post("/", function(req, res) {
+//     var data = {
+//         email: req.body.email,
+//         password: crypto(req.body.email + req.body.password).toString(),
+//     };
+
+//     console.log(data);
+
+//     var options = {
+//         uri: "http://localhost:8080/user/login",
+//         json: data,
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//     };
+
+//     request(options, function(error, response) {
+//         // global.cookie = response.headers["set-cookie"];
+//         if (response) {
+//             res.send(response.body);
+//         }
+//         return;
+//     });
+// });
+
+// module.exports = router;
